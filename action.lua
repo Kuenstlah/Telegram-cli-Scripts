47f9bae6608d970429d93e7ebfc0f5bb14b99b13
Kuenstlah <Kevin@Kuenstlah.de>
2016-09-04 16:50:00 +0200

    Added Kat

--- action.lua
+++ action.lua
@@ -1,6 +1,6 @@
 -- Users must be added manually.
 -- Lower number = higher permissions
-local users = { "1_Kuenstlah", "1_Phil", "1_Pascal", "2_Silvan", "2_Rrobi", "2_Sascha", "2_Dom" }
+local users = { "1_Kuenstlah", "1_Phil", "1_Pascal", "2_Silvan", "2_Rrobi", "2_Sascha", "2_Dom", "2_Kat" }
 
 function has_value (tab, val)
     for index, value in ipairs (tab) do
