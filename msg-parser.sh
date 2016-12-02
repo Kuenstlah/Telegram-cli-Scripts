a103a0cc7c517bf9e113d75a8fe62900529d1250
Kuenstlah <Kevin@Kuenstlah.de>
2016-12-02 21:04:37 +0100

    Update

--- msg-parser.sh
+++ msg-parser.sh
@@ -24,61 +24,65 @@ msg="${1,,}"
 
 level=${empf:0:1}
 case $msg in
-	p|ping)
+	ping)
 		echo "msg $empf Pong" | $cmd
 	;;
-	u|uptime)
+	uptime)
 		echo "msg $empf "$(uptime)"" | $cmd
 	;;
-        st|status)
+        status)
                 echo "msg $empf "Access level: $level \(1 = Admin\)"" | $cmd
         ;;
         s|ps|pgospawn)
 		echo "msg $empf "### The classical spawn does not exist anymore! Please use one of the following commands:"" | $cmd
-		echo "msg $empf "### [sh] - Pokespawns in Hilden"" | $cmd
-                echo "msg $empf "### [sg] - Pokespawns in Garath"" | $cmd
+		echo "msg $empf "### [sh] - Pokespawns in H"" | $cmd
+		echo "msg $empf "### [sg] - Pokespawns in G"" | $cmd
+		echo "msg $empf "### [st] - Pokespawns in Tmp"" | $cmd
         ;;
         sh|psh|pgospawnh)
-                echo "msg $empf "### Searching in Hilden, please wait..."" | $cmd
-                `/home/pi/pokemon/pogo/checkdb.pl spawn_hilden $empf`
+                echo "msg $empf "### Searching, please wait..."" | $cmd
+                `/home/pi/pokemon/pogo/checkdb.pl spawn_h $empf`
                 echo "msg $empf "### Done. Good luck!"" | $cmd
         ;;
         sg|psg|pgospawng)
-                echo "msg $empf "### Searching in Garath, please wait..."" | $cmd
-                `/home/pi/pokemon/pogo/checkdb.pl spawn_garath $empf &`
+                echo "msg $empf "### Searching, please wait..."" | $cmd
+                `/home/pi/pokemon/pogo/checkdb.pl spawn_g $empf &`
                 echo "msg $empf "### Done. Good luck!"" | $cmd
         ;;
-        sr|psr|pgospawnr)
-                echo "msg $empf "### Searching in Ratingen, please wait..."" | $cmd
-                `/home/pi/pokemon/pogo/checkdb.pl spawn_ratingen $empf &`
+        st|pst|pgospawnt)
+                echo "msg $empf "### Searching, please wait..."" | $cmd
+                `/home/pi/pokemon/pogo/checkdb.pl spawn_tmp $empf &`
                 echo "msg $empf "### Done. Good luck!"" | $cmd
         ;;
 	stats)
 		if [[ $level == 1 ]];then
 			echo "msg $empf "### Usage of commands:"" | $cmd
-			echo "msg $empf "`/home/pi/pokemon/stats.sh`"" | $cmd
+			echo "msg $empf "`/home/pi/tg/scripts/stats.sh`"" | $cmd
 		else
                         echo "msg $empf "Sorry, your accesslevel \($level\) is too low."" | $cmd
                 fi
 	;;
- 	map|map*)
+ 	mapstart)
 		if [[ $level == 1 ]];then
-				location=`echo $msg |awk -F ' ' {'print $2'}`
-				if [[ $location == "status" ]];then
-					map_status
-				else
-					echo "msg $empf "### Restarting map, please wait..."" | $cmd
-					/home/pi/restart_pokemon.sh -l "$location"
-					echo "msg $empf "### Done. Set position to $location! Please do not use this function for the next 2 minutes."" | $cmd
-					map_status
-				fi
+				echo "msg $empf "### Starting map, please wait..."" | $cmd
+				/home/pi/scripts/pogo/start.sh
+				echo "msg $empf "### Done"" | $cmd
 		else
 			echo "msg $empf "Sorry, your accesslevel \($level\) is too low."" | $cmd
 		fi
         ;;
+        mapstop)
+                if [[ $level == 1 ]];then
+                                echo "msg $empf "### Stopping map, please wait..."" | $cmd
+                                killall screen
+                                echo "msg $empf "### Done"" | $cmd
+                else
+                        echo "msg $empf "Sorry, your accesslevel \($level\) is too low."" | $cmd
+                fi
+        ;;
 	c|camera|k|kamera|w|webcam)
 		if [[ $level == 1 ]];then
-			echo "msg $empf "Give me a second to spy.."" | $cmd
+			echo "msg $empf "Trying to get latest picture.."" | $cmd
 			# Trigger a snapshot via motion from local webcam
 			#curl -s -o /dev/null http://localhost:8080/0/action/snapshot
 			#sleep 1
@@ -88,7 +92,7 @@ case $msg in
 			echo "msg $empf "Sorry, your accesslevel \($level\) is too low."" | $cmd
 		fi
 	;;
-        pir|raspi-reboot)
+        raspi-reboot)
                 if [[ $empf == "1_Kuenstlah" ]];then
 			echo "msg $empf "Rebooting now"" | $cmd
 	                reboot
@@ -99,11 +103,14 @@ case $msg in
 
         h|hilfe)
                 echo -e "msg $empf "Tell me following [words] or [shortcuts]:"" | $cmd
-                echo -e "msg $empf "[st]atus - Shows your access level"" | $cmd
+                echo -e "msg $empf "status - Shows your access level"" | $cmd
+                echo -e "msg $empf "uptime - Shows uptime"" | $cmd
+                echo -e "msg $empf "stats - Shows usage of commands"" | $cmd
                 echo -e "msg $empf "[w]ebcam - Live picture from webcam"" | $cmd
-                echo -e "msg $empf "ras[pi]-[r]eboot - Restarts the server"" | $cmd
-                echo -e "msg $empf "[s]pawn - Show currently spawned Pokemons"" | $cmd
-                echo -e "msg $empf "[Map LOCATION]- Restarts the map with desired location."" | $cmd
+                echo -e "msg $empf "[raspi-reboot] - Restarts the server"" | $cmd
+                echo -e "msg $empf "[s]pawn - Show current spawns"" | $cmd
+		echo -e "msg $empf "[mapstart/stop] - Start/Stops Map"" | $cmd
+#                echo -e "msg $empf "[Map LOCATION]- Restarts the map with desired location."" | $cmd
         ;;
 	*)
 		echo "msg $empf 'I don't know this command, please write 'h' or 'help''" | $cmd
